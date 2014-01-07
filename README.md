8f6e8c4b546f46218cee63f48f942f88965f13b3
Dan Prince <dan@codecurve.net>
2014-01-07 21:30:50 +0000

    First version

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-baptist
-=======
+font-bomb
+=========
 
-Serve Google Web Fonts Yourself
+_Serve Google Web Fonts Yourself_
+
+A small tool designed to be used with Google WebFonts. Basically, I got a bit fed up of developing web apps on lousy connections and having my fonts not load because they were hosted on Google, so I wrote this tool to save anyone else the trouble.
+
+---
+
+Here's how it works.
+
+```
+npm install fontbomb -g
+cd public/
+fontbomb "http://fonts.googleapis.com/css?family=Seymour+One|Chango"
+```
+
+Fontbomb will then go away and collect all required Woff and CSS files and add them straight into your project*.
+
+Then just reference the stylesheet created at `css/fonts.css` from your HTML file, and you'll be able to use all of those new fonts, but you'll be serving them yourself.
+
+\* Fontbomb will create the folders if they aren't there. 
+
---
+++ fontBomb.js
@@ -0,0 +1,76 @@
+var request = require('request')
+  , fs = require('fs')
+  , mkdirp = require('mkdirp');
+
+var fonts = 'fonts';
+var makeWoffPath = makePath('fonts', 'woff');
+var makeCSSPath = makePath('css', 'css');
+
+function main() {
+  var input, url;
+  // get our input 
+  input = process.argv[2];
+  
+  // extract url
+  url = input.match(/(http:\/\/.*?)['\s]/);
+  
+  // is there a url?
+  if(url && url[1]) {
+    getCSS(url[1]);
+  } else {
+    throw new Error('Could not find a valid URL.')
+  }
+}
+
+function makePath(folder, extension) {
+  // just in case it doesn't exist
+  mkdirp(folder);
+  
+  return function(file) {
+    return folder + '/' + file + '.' + extension;
+  }
+}
+
+function getCSS(url) {
+  var matches;
+  console.log('Getting CSS...');
+  
+  request(url, function(err, res, body) {
+    if(err) throw err;
+    var css, cssPath;
+
+    css = parseCSS(body);
+    cssPath = makeCSSPath('fonts');
+
+    console.log('Adding new css file at', cssPath);
+    fs.writeFile(cssPath, css);
+  });
+}
+
+function parseCSS(css) {
+  var re, match, url, name, woffPath;
+  console.log('Parsing CSS...'); 
+
+  // capture the name of the font and its URL  
+  re = /src: local.* local\('(.*?)'\).*url\((.*?)\)/g;
+  while(match = re.exec(css)) {
+    name = match[1];
+    url = match[2];
+    woffPath = makeWoffPath(name);
+    css = css.replace(url, woffPath);
+    saveWoff(url, woffPath);
+  }
+  
+  return css;
+}
+
+function saveWoff(url, destination) {
+  console.log('Get Woff from', url);
+  request(url, function(err, res, body) {
+    if(err) throw err;
+    console.log('Adding new woff file at', destination);
+    fs.writeFile(destination, body);
+  });
+}
+
+main();
